ee98c7c0fd4ee6740a6372ffce5d865370f34901
reboot-phoenix <bscit.tiu2012@gmail.com>
2026-07-04 22:01:11 +0530

    Add files via upload

--- checker.py
+++ checker.py
@@ -1,24 +1,17 @@
-from modules.name import check_name
-from modules.phone import check_phone
-from modules.email import check_email
+from modules.name     import check_name
+from modules.phone    import check_phone
+from modules.email    import check_email
+from modules.username import check_username
 
 
 def scan(scan_type, query):
-    """
-    Main OSINT dispatcher.
-    Every module returns:
-        results, score, summary
-    """
-
     scan_type = scan_type.lower().strip()
-
     scanners = {
-        "name": check_name,
-        "phone": check_phone,
-        "email": check_email,
+        "name":     check_name,
+        "phone":    check_phone,
+        "email":    check_email,
+        "username": check_username,
     }
-
     if scan_type not in scanners:
         raise ValueError(f"Unsupported scan type: {scan_type}")
-
     return scanners[scan_type](query)
